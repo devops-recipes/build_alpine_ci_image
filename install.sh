82b49f819982fe8aa581415e313c579e87aea616
Avi Cavale <avi@shippable.com>
2018-06-01 15:45:45 +0530

    adding apk update

--- install.sh
+++ install.sh
@@ -20,6 +20,14 @@ apk update
 echo "================ installing packages ======================="
 apk add --no-cache $PACKAGES
 
+
+echo "================ installing glibc ======================="
+apk --no-cache add ca-certificates wget
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
+apk add glibc-2.27-r0.apk
+
+
 echo "================ make some useful symlinks that are expected to exist ======================="
 if [[ ! -e /usr/bin/python ]];        then ln -sf /usr/bin/python2.7 /usr/bin/python; fi
 if [[ ! -e /usr/bin/python-config ]]; then ln -sf /usr/bin/python2.7-config /usr/bin/python-config; fi
