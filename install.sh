01dc6cce21125a22244b4a4f611a80e8b9d83e31
Avi Cavale <avi@shippable.com>
2018-06-01 13:57:44 +0530

    intial commit

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+export PACKAGES="\
+    coreutils \
+    sudo \
+    git \
+    openssh-client \
+    ca-certificates \
+    musl \
+    linux-headers \
+    build-base \
+    python2 \
+    python2-dev \
+    py-pip \
+    nodejs \
+"
+
+
+echo "================ installing packages ======================="
+apk add --no-cache $PACKAGES
+
+echo "================ make some useful symlinks that are expected to exist ======================="
+if [[ ! -e /usr/bin/python ]];        then ln -sf /usr/bin/python2.7 /usr/bin/python; fi
+if [[ ! -e /usr/bin/python-config ]]; then ln -sf /usr/bin/python2.7-config /usr/bin/python-config; fi
+if [[ ! -e /usr/bin/easy_install ]];  then ln -sf /usr/bin/easy_install-2.7 /usr/bin/easy_install; fi
+
+echo "================ install and upgrade pip ======================="
+easy_install pip
+pip install --upgrade pip
+if [[ ! -e /usr/bin/pip ]]; then ln -sf /usr/bin/pip2.7 /usr/bin/pip; fi
+
+echo "================ test ssh ======================="
+eval `ssh-agent -s`
