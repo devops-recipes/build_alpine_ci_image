f56438f95203fe7ee2c25419f6a18ea1f581517b
Avi Cavale <avi@shippable.com>
2018-06-04 09:55:32 +0530

    adding apk update

--- install.sh
+++ install.sh
@@ -25,10 +25,15 @@ echo "================ installing glibc ======================="
 apk --no-cache add ca-certificates wget
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
-apk add glibc-2.27-r0.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
 
-apk add glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk
+apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk
+
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
+
+#/usr/glibc-compat/sbin/ldconfig /lib /usr/glibc-compat/lib
+#echo 'hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4' >> /etc/nsswitch.conf
 
 echo "================ make some useful symlinks that are expected to exist ======================="
 if [[ ! -e /usr/bin/python ]];        then ln -sf /usr/bin/python2.7 /usr/bin/python; fi
