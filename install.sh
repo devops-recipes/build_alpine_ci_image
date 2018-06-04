1e3c16401b1c9a7282da89defdb90e89213b9b23
Avi Cavale <avi@shippable.com>
2018-06-04 10:03:12 +0530

    cutting locales

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ echo "================ installing glibc ======================="
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk
-#wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
 
-apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk #glibc-i18n-2.27-r0.apk
+apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
 
 echo "================ make some useful symlinks that are expected to exist ======================="
