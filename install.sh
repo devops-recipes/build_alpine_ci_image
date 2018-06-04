9094fa1247cc2c2f2ca8b30845921e61983bcb76
Avi Cavale <avi@shippable.com>
2018-06-04 10:01:59 +0530

    cutting locales

--- install.sh
+++ install.sh
@@ -12,13 +12,11 @@ export PACKAGES="\
     python2 \
     python2-dev \
     py-pip \
-    nodejs \
     wget \
 "
 
-apk update
-
 echo "================ installing packages ======================="
+apk update
 apk add --no-cache $PACKAGES
 
 
@@ -26,14 +24,10 @@ echo "================ installing glibc ======================="
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
+#wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
 
-apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk
-
+apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk #glibc-i18n-2.27-r0.apk
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
-
-#/usr/glibc-compat/sbin/ldconfig /lib /usr/glibc-compat/lib
-#echo 'hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4' >> /etc/nsswitch.conf
 
 echo "================ make some useful symlinks that are expected to exist ======================="
 if [[ ! -e /usr/bin/python ]];        then ln -sf /usr/bin/python2.7 /usr/bin/python; fi
