c99ff026fa3f1ee8933166e3f88b90c3b7752252
John Stockbauer <john@shippable.com>
2018-10-03 16:28:47 -0700

    fix sgerrand glibc public key url

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ apk add --no-cache $PACKAGES
 
 
 echo "================ installing glibc from sgerrand======================="
-wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
